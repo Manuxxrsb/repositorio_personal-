7d61e14eb869501eb603395e5b1695fbeab4b312
Manuxxrb <manuxxrsb@gmail.com>
2023-10-31 12:55:50 -0300

    Agrego Funciones
    e agregaron la funciones suma y resta a modo de ejemplo y terminar de3 acostumbrarme a GitHub desktop

--- Librerias y funciones/Ejemplo de main.c
+++ Librerias y funciones/Ejemplo de main.c
@@ -2,14 +2,27 @@
 #include <stdio.h>
 
 int suma(int,int);
+int resta(int,int);
 
 int main(){
 
     printf("Hola mundo!");
+
+    //llamada a suma
+    int a =suma(1,3);
+    printf("\nEl valor de suma fue %d ",a);
+
+    //llamada a resta
+    int b = resta(10,6);
+    printf("\nEl valor de la resta fue %d",b);
 
     return 0;
 }
 
 int suma(int a,int b){
     return a+b;
+}
+
+int resta(int a,int b){
+    return a-b;
 }
